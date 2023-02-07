e8068e48f9c881bbc4981425bc4df7bac84dc6f0
Dane Bulat <dane.bulat@gmail.com>
2023-02-07 18:10:52 +0000

    chapter 3 exercises

--- exercises/chapter3/test/MySolutions.purs
+++ exercises/chapter3/test/MySolutions.purs
@@ -2,4 +2,26 @@ module Test.MySolutions where
 
 import Prelude
 
--- Note to reader: Add your solutions to this file
+import Data.AddressBook
+import Data.List  (head, filter, nubByEq)
+import Data.Maybe (isJust, Maybe)
+
+-- Return entry based on street name
+findEntryByStreet :: String -> AddressBook -> Maybe Entry
+findEntryByStreet street = head <<< filter filterStreet
+  where 
+    filterStreet :: Entry -> Boolean
+    filterStreet entry = entry.address.street == street
+
+-- Check if name appears in an address book
+isInBook :: String -> String -> AddressBook -> Boolean
+isInBook firstName lastName = isJust <<< findEntry firstName lastName 
+
+-- Remove duplicate address book entries
+removeDuplicates :: AddressBook -> AddressBook
+removeDuplicates book = nubByEq pred book
+  where 
+    pred :: Entry -> Entry -> Boolean
+    pred e1 e2 = e1.firstName == e2.firstName &&
+                 e1.lastName  == e2.lastName
+
